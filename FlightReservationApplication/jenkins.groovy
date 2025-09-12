b599ccbe3597a10d2d5bb886d621cef2152f883f
mayurwagh <mayurwagh1412@gmail.com>
2025-09-12 21:28:00 +0530

    error resolved

--- FlightReservationApplication/jenkins.groovy
+++ FlightReservationApplication/jenkins.groovy
@@ -2,7 +2,7 @@ pipeline{
     agent any 
     stages{
         stage('Pull'){
-            step{
+            steps{
                 git branch: 'main', url: 'https://github.com/mayurmwagh/Flight-reservation-pls-7-8.git'
             }
         }
