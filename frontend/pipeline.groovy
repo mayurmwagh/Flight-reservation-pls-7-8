3dcb875015a2aee129b69b117fbbaa72130036e4
mayurwagh <mayurwagh1412@gmail.com>
2025-09-15 21:47:49 +0530

    errror fixed

--- frontend/pipeline.groovy
+++ frontend/pipeline.groovy
@@ -11,7 +11,7 @@ pipeline {
                 sh '''
                     cd frontend
                     npm install
-                    npm build 
+                    npm run build 
                 '''
             }
         }
